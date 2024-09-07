1a4fd0c41daf6516f3bb970f4e67e3295cece238
kyungbinYoo <ykb0314y@naver.com>
2024-09-07 16:24:53 +0900

    Initial commit

---
+++ src/main/java/com/springboot/anding/AndingApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.anding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/anding/AndingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.anding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
